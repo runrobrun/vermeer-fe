ad4fc0a668170ea96659145e043c97e6a5ee2166
Rob Hammann <runrobrun@gmail.com>
2015-06-03 09:30:29 -0500

    adding grunt updates for sass

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,19 @@
-require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks 
+module.exports = function (grunt) {
+
+	require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks 
  
-grunt.initConfig({
-    sass: {
-        options: {
-            sourceMap: true
-        },
-        dist: {
-            files: {
-                'main.css': 'main.scss'
-            }
-        }
-    }
-});
- 
-grunt.registerTask('default', ['sass']);
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'main.css': 'main.scss'
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('default', ['sass']);
+};
